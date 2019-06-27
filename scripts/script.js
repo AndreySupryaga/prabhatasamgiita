b1573aae611c24c2b18b1a86068e728ce0edfa03
Andrey Supryaga <a.supryaga@smartlinegroup.eu>
2019-06-27 17:24:16 +0300

    Added awesomecomlete.

--- scripts/script.js
+++ scripts/script.js
@@ -5,18 +5,20 @@ $(function () {
         original: $('.original-text'),
         tabEng: $('.tab-eng-container'),
         tabRu: $('.tab-ru-container'),
-        video: $('.video')
+        video: $('.video'),
+        input: $('#autocompele-input')
     };
 
-    $.getJSON( "poems.json", function( data ) {
+    $.getJSON("poems.json", function (data) {
         console.log(data);
         const poem = data[0];
         setPoemToMarkup(poem);
+        awesomeCompleteInit(data);
     });
 
 
-    function setPoemToMarkup (poem) {
-        el.title[0].innerHTML = poem.number;
+    function setPoemToMarkup(poem) {
+        el.title[0].innerHTML = poem.value;
         el.original[0].innerHTML = poem.text;
         if (poem.locale.ru) {
             el.tabRu[0].innerHTML = poem.locale.ru;
@@ -28,6 +30,34 @@ $(function () {
             el.video[0].innerHTML = poem.video;
         }
         el.original[0].innerHTML = poem.text;
+    }
+
+    function awesomeCompleteInit(list) {
+        const comboplete = new Awesomplete('#autocompele-input', {
+            minChars: 0,
+            list: list,
+            autoFirst: true,
+            maxItems: 20,
+            select: function (item) {
+                console.log(item);
+            }
+        });
+
+        Awesomplete.$('.dropdown-btn').addEventListener("click", function () {
+            if (comboplete.ul.childNodes.length === 0) {
+                comboplete.minChars = 0;
+                comboplete.evaluate();
+            } else if (comboplete.ul.hasAttribute('hidden')) {
+                comboplete.open();
+            } else {
+                comboplete.close();
+            }
+        });
+
+        comboplete.select(function (item) {
+                console.log(item);
+            }
+        )
     }
 
 });
